1a734a3ef9d9b1de78fcf539a2d0724f2170bce6
cmontminy <colette.montminy@gmail.com>
2019-12-07 17:53:36 +0000

    Added old version of startTelemetry script so it can be used with the old udp system

---
+++ Raspberry Pi Telemetry Server/startTelemetry-old.sh
@@ -0,0 +1,23 @@
+echo "Enter Date and time in UTC time, ex: Sep 20 15:45:00"
+echo "Note that UTC time is 5 hours ahead of central standard time (Texas time), so just add 5 hours"
+read dateTime
+sudo date -s "$dateTime"
+sudo service hostapd stop
+sudo ifconfig wlan0 down
+echo "Stopped hostadp and shutdown wlan0"
+sleep 5
+sudo ifconfig wlan0 192.168.1.1 up
+sudo ifconfig eth0 192.168.4.2 up
+echo "Restarted wlan0"
+sudo service dnsmasq start
+echo "Launched dnsmasq"
+sudo service hostapd start
+echo "Launched hostapd"
+sudo service grafana-server start
+echo "Launched grafana-server"
+lxterminal -e influx
+echo "Launched influx server"
+lxterminal -e python3 ./datagen.py
+echo "Launched python upd socket"
+echo "Settings files are located in /etc/dnsmasq.conf and /etc/hostapd/hostapd.conf"
+
